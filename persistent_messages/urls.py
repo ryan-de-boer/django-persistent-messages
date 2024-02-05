10acb1b92933995eaadb74e6c7de225a5d2f9b38
Ryan De Boer <ryandeboer@amristar.com>
2024-02-05 09:41:20 +0800

    Switch back to url for Django 1.11 compatibility

--- persistent_messages/urls.py
+++ persistent_messages/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path, re_path
+from django.conf.urls import url
 from persistent_messages.views import message_detail, message_mark_read, message_mark_all_read
 
 urlpatterns = [
-    path('detail/<int:message_id>/', message_detail, name='message_detail'),
-    re_path(r'^mark_read/(?P<message_id>\d+)/$', message_mark_read, name='message_mark_read'),
-    path('mark_read/all/', message_mark_all_read, name='message_mark_all_read'),
+    url('detail/<int:message_id>/', message_detail, name='message_detail'),
+    url(r'^mark_read/(?P<message_id>\d+)/$', message_mark_read, name='message_mark_read'),
+    url('mark_read/all/', message_mark_all_read, name='message_mark_all_read'),
 ]
