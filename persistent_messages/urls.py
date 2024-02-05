4c83440501fbdb2bfa857fba8475f915861bbd5f
Ryan De Boer <ryandeboer@amristar.com>
2024-02-05 09:37:47 +0800

    Convert url paths to Django 1.11 syntax

--- persistent_messages/urls.py
+++ persistent_messages/urls.py
@@ -1,7 +1,8 @@
-from django.conf.urls import *
+from django.urls import path, re_path
+from persistent_messages.views import message_detail, message_mark_read, message_mark_all_read
 
-urlpatterns = ['',
-    url(r'^detail/(?P<message_id>\d+)/$', 'persistent_messages.views.message_detail', name='message_detail'),
-    url(r'^mark_read/(?P<message_id>\d+)/$', 'persistent_messages.views.message_mark_read', name='message_mark_read'),
-    url(r'^mark_read/all/$', 'persistent_messages.views.message_mark_all_read', name='message_mark_all_read'),
+urlpatterns = [
+    path('detail/<int:message_id>/', message_detail, name='message_detail'),
+    re_path(r'^mark_read/(?P<message_id>\d+)/$', message_mark_read, name='message_mark_read'),
+    path('mark_read/all/', message_mark_all_read, name='message_mark_all_read'),
 ]
